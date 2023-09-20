0e12d511db8c6a7872b25a2d19d51044000de2c4
Sushankznation <sushantbharwal@gmail.com>
2023-09-20 11:54:00 +0530

    BAsic Structure done

--- app/(dashboard)/(routes)/dashboard/page.tsx
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -1,8 +1,13 @@
-import { Button } from "@/components/ui/button";
-export default function Dashboard (){
-  return(
-    <div> Hello 
-      <Button variant="destructive">Page After SIgn in The Things</Button>
+const DashboardLayout = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <div className="h-full relative">
+      <div className=" hidden h-full md:flex md:w-72 md:flex-col md:fixed md:inset-y-0 z-[80] bg-gray-900">
+        <div className="text-white">Hello Sidebar</div>
+      </div>
+      <main className="md:pl-72">
+        Hello Content
+      </main>
     </div>
-  )
-}
+  );
+};
+export default DashboardLayout;
