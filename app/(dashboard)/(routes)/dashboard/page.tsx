68b12133ac49a50833e4c2d2324e3829313a8b32
Sushankznation <sushantbharwal@gmail.com>
2023-09-20 13:03:06 +0530

    added Images in my App

--- app/(dashboard)/(routes)/dashboard/page.tsx
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -1,13 +1,10 @@
-const DashboardLayout = ({ children }: { children: React.ReactNode }) => {
+const DashboardPage = () => {
   return (
-    <div className="h-full relative">
-      <div className=" hidden h-full md:flex md:w-72 md:flex-col md:fixed md:inset-y-0 z-[80] bg-gray-900">
-        <div className="text-white">Hello Sidebar</div>
+    <>
+      <div>
+        dashBoard Page Protected
       </div>
-      <main className="md:pl-72">
-        Hello Content
-      </main>
-    </div>
+    </>
   );
 };
-export default DashboardLayout;
+export default DashboardPage;
