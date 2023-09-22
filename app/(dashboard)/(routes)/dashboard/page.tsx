f2968bf311f6762de9c8b1db31107e6270237c18
Sushankznation <sushantbharwal@gmail.com>
2023-09-22 14:53:12 +0530

    added all the links on dashboard

--- app/(dashboard)/(routes)/dashboard/page.tsx
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -1,4 +1,54 @@
-'use client'
+"use client";
+
+import { Card } from "@/components/ui/card";
+import { cn } from "@/lib/utils";
+import {
+  ArrowRight,
+  Code,
+  ImageIcon,
+  MessageSquare,
+  Music2Icon,
+  VideoIcon,
+} from "lucide-react";
+
+const tools = [
+  {
+    label: "Conversation",
+    icon: MessageSquare,
+    color: "text-violet-500",
+    bgColor: "bg-violet-500/10",
+    href: "/conversation",
+  },
+  {
+    label: "Video Generator",
+    icon: VideoIcon,
+    color: "text-orange-300",
+    bgColor: "bg-orange-300/10",
+    href: "/video-generator",
+  },
+  {
+    label: "Image Generator",
+    icon: ImageIcon,
+    color: "text-pink-700",
+    bgColor: "bg-violet-pink-700/10",
+    href: "/image-generator",
+  },
+  {
+    label: "Music",
+    icon: Music2Icon,
+    color: "text-emerald-500",
+    bgColor: "bg-emerald-500/10",
+    href: "/music",
+  },
+  {
+    label: "Code Generator",
+    icon: Code,
+    color: "text-green-700",
+    bgColor: "bg-green-500/10",
+    href: "/code-generator",
+  },
+];
+
 const DashboardPage = () => {
   return (
     <>
@@ -11,7 +61,22 @@ const DashboardPage = () => {
           अज्ञानतिमिरान्धस्य ज्ञानाञ्जनशलाकया। चक्षुरुन्मीलितं येन तस्मै
           श्रीगुरवे नमः॥
         </p>
-        <div className="px-4 md:px-20 lg:px-32 space-y-4"></div>
+        <div className="px-4 md:px-20 lg:px-32 space-y-4">
+          {tools.map((tool) => (
+            <Card
+              key={tool.href}
+              className="p-4 border-black/5 flex items-center justify-between hover:shadow-md cursor-pointer"
+            >
+              <div className="flex items-center gap-x-4">
+                <div className={cn("p-2 w-fit rounded-md", tool.bgColor)}>
+                  <tool.icon className={cn("w-8 h-8", tool.color)} />
+                </div>
+                <div className="font-semibold">{tool.label}</div>
+              </div>
+              <ArrowRight className="w-5 h-5" />
+            </Card>
+          ))}
+        </div>
       </div>
     </>
   );
