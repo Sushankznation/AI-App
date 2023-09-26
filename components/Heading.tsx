b8d34280067d7bf3e3054c9f39cdcde30eef0c4e
sushankznation <sushantbharwal@gmail.com>
2023-09-26 18:05:17 +0530

    haedinf Done

--- components/Heading.tsx
+++ components/Heading.tsx
@@ -11,7 +11,7 @@ interface HeadingProps {
 export default function Heading({
   title,
   description,
-  icon : Icon,
+  icon: Icon,
   iconColor,
   bgColor,
 }: HeadingProps) {
@@ -19,8 +19,12 @@ export default function Heading({
     <>
       <div className="px-4 lg:px-8 flex items-center gap-x-3 mb-8">
         <div className={cn("p-2 w-fit rounded-md", bgColor)}>
-            <Icon className={cn("w-10 h-10",iconColor)}/>
+          <Icon className={cn("w-10 h-10", iconColor)} />
         </div>
+      </div>
+      <div>
+        <h2 className="text-3xl font-bold">{title}</h2>
+        <p className="text-sm text-muted-foreground">{description}</p>
       </div>
     </>
   );
